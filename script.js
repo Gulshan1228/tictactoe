d9af9b74ea9841dc41b2620dd9f2120b0054b9ed
Gulshan Singh <126390444+Gulshan1228@users.noreply.github.com>
2024-03-09 20:38:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let boxs = document.querySelectorAll(".box")
+let btns = document.querySelectorAll(".btn")
 let body = document.querySelector("body")
 let player1 = true
 let Winnner = document.querySelector(".winner")
@@ -31,6 +32,15 @@ boxs.forEach((box) => {
             }
         }
         // console.log(count)
+        let dis = 0
+        if( Winnner.innerHTML != "" ){
+            dis++
+        }
+        for(let i of boxs){
+            if(dis !=0 ){
+                i.disabled=true;
+            }
+        }
     })
 })
 
@@ -42,8 +52,8 @@ let checkWinner=() => {
         if( boxs[0].innerHTML === boxs[1].innerHTML && boxs[2].innerHTML == boxs[0].innerHTML){
             console.log(` ${boxs[0].innerHTML} is The Winnner `)
             Winnner.innerHTML=`Congratulations , ${boxs[0].innerHTML} is Winner`
+            body.style.height="115vh"
             boxs.disabled=true
-                body.style.height="115vh"
         }
     }
 
@@ -51,8 +61,8 @@ let checkWinner=() => {
         if( boxs[3].innerHTML === boxs[4].innerHTML && boxs[5].innerHTML == boxs[3].innerHTML){
             console.log(` ${boxs[3].innerHTML} is The Winnner `)
             Winnner.innerHTML=`Congratulations , ${boxs[3].innerHTML} is Winner`
+            body.style.height="115vh"
             boxs.disabled=true
-                body.style.height="115vh"
         }
     } 
 
@@ -60,8 +70,8 @@ let checkWinner=() => {
         if( boxs[6].innerHTML === boxs[7].innerHTML && boxs[8].innerHTML == boxs[6].innerHTML){
             console.log(` ${boxs[6].innerHTML} is The Winnner `)
             Winnner.innerHTML=`Congratulations , ${boxs[6].innerHTML} is Winner`
+            body.style.height="115vh"
             boxs.disabled=true
-                body.style.height="115vh"
         }
     }
 
@@ -69,8 +79,8 @@ let checkWinner=() => {
         if( boxs[0].innerHTML === boxs[3].innerHTML && boxs[6].innerHTML == boxs[0].innerHTML){
             console.log(` ${boxs[3].innerHTML} is The Winnner `)
             Winnner.innerHTML=`Congratulations , ${boxs[3].innerHTML} is Winner`
+            body.style.height="115vh"
             boxs.disabled=true
-                body.style.height="115vh"
         }
     }
 
@@ -78,8 +88,8 @@ let checkWinner=() => {
         if( boxs[1].innerHTML === boxs[4].innerHTML && boxs[7].innerHTML == boxs[1].innerHTML){
             console.log(` ${boxs[4].innerHTML} is The Winnner `)
             Winnner.innerHTML=`Congratulations , ${boxs[4].innerHTML} is Winner`
+            body.style.height="115vh"
             boxs.disabled=true
-                body.style.height="115vh"
         }
     }
     
@@ -87,16 +97,16 @@ let checkWinner=() => {
         if( boxs[2].innerHTML === boxs[5].innerHTML && boxs[8].innerHTML == boxs[2].innerHTML){
             console.log(` ${boxs[2].innerHTML} is The Winnner `)
             Winnner.innerHTML=`Congratulations , ${boxs[2].innerHTML} is Winner`
+            body.style.height="115vh"
             boxs.disabled=true
-                body.style.height="115vh"
         }
     }
      if ( boxs[2].innerHTML!='' && boxs[4].innerHTML!='' && boxs[6].innerHTML!='' ){
         if( boxs[2].innerHTML === boxs[4].innerHTML && boxs[6].innerHTML == boxs[2].innerHTML){
             console.log(` ${boxs[2].innerHTML} is The Winnner `)
             Winnner.innerHTML=`Congratulations , ${boxs[2].innerHTML} is Winner`
+            body.style.height="115vh"
             boxs.disabled=true
-                body.style.height="115vh"
 
         }
     }
@@ -104,8 +114,8 @@ let checkWinner=() => {
         if( boxs[0].innerHTML === boxs[4].innerHTML && boxs[8].innerHTML == boxs[0].innerHTML){
             console.log(` ${boxs[4].innerHTML} is The Winnner `)
             Winnner.innerHTML=`Congratulations , ${boxs[4].innerHTML} is Winner`
+            body.style.height="115vh"
             boxs.disabled=true
-                body.style.height="115vh"
         }
     }
 
